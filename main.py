744b1a62143d531636cb874ab85ea4a8e1fa10d5
ClaireChilders <clairechilders@oakland.edu>
2023-11-27 14:26:19 -0500

    feat: add logging

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from datetime import datetime as dt
+
+import logging
+logfile = f"logs/{dt.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+logging.basicConfig(filename=logfile, encoding='utf-8', level=logging.DEBUG, filemode='w', format='%(levelname)s:%(asctime)s:[%(module)s] %(message)s')
+from SurveyResults import SurveyResults
+
+
 class Driver():
     def __init__(self):
         self._survey_results = SurveyResults()
